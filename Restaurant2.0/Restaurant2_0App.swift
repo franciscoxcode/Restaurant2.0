da9a4b5739cbd86d0a9ebd9f2ab0535a3b4d3c0a
Francisco Casillas <chiccasillas@gmail.com>
2025-07-25 10:19:04 -0600

    Initial Commit

---
+++ Restaurant2.0/Restaurant2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant2_0App.swift
+//  Restaurant2.0
+//
+//  Created by Francisco Jean on 25/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
